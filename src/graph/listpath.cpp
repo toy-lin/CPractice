a862bc96805386c415a49fbda88e4f2dfad46b92
linkaitao <linkaitao@qiku.com>
2017-06-19 21:58:42 +0800

    update listpath

--- src/graph/listpath.cpp
+++ src/graph/listpath.cpp
@@ -4,7 +4,11 @@
 using namespace std;
 
 void listpath(ALGraph &g,int from ,int to);
-void findAndListPath(ALgraph &g,int from,int to,bool* visited,int* visitList);
+void findAndListPath(ALGraph &g,int from,int to,bool* visited,int* visitList);
+void listAdd(int* list,int data);
+void listRemoveLast(int* list);
+void listPrint(int* list);
+
 int main(){
 	ALGraph g = makeALGraph();
 	int from,to;
@@ -12,7 +16,7 @@ int main(){
 	cin>>from;
 	cout<<"input to:";
 	cin>>to;
-	listPath(g,from,to);
+	listpath(g,from,to);
 }
 
 void listpath(ALGraph &g , int from ,int to){
@@ -29,10 +33,53 @@ void listpath(ALGraph &g , int from ,int to){
 }
 
 void findAndListPath(ALGraph &g ,int from ,int to,bool* visited,int* visitList){
-	visit(visitList,from);
-	VNode* v = g.vetices[from];
+	if(visited[from])
+		return;
+
+	visited[from] = true;
+	listAdd(visitList,from);
+	VNode* v = &g.vertices[from];
 	ArcNode* a = v->first;
+
 	while(a != NULL){
-		
+		if(a->adjvex == to){
+			listAdd(visitList,to);
+			listPrint(visitList);
+			listRemoveLast(visitList);
+		}else{
+			findAndListPath(g,a->adjvex,to,visited,visitList);
+		}
+		a = a->next;
 	}
+	visited[from] = false;
+	listRemoveLast(visitList);
+}
+
+void listAdd(int *visitList,int data){
+	for(int i =0 ; i< 100; i++){
+		if(visitList[i] == -1){
+			visitList[i] = data;
+			return;
+		}
+	}
+}
+
+void listRemoveLast(int* visitList){
+	for(int i =0 ; i< 100; i++){
+		if(visitList[i] ==-1){
+			visitList[i-1] = -1;
+			return;
+		}
+	}
+}
+
+void listPrint(int* visitList){	
+	for(int i =0 ; i< 100; i++){
+		if(visitList[i]!=-1){
+			cout<< visitList[i] << "->";
+		}else{
+			break;
+		}
+	}
+	cout << "\n";
 }
